81ea040d1c90ae6c92fcaae15056f2ef06ed8bd8
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2026-02-14 15:59:21 +0330

    implement jwt token generator and validator

---
+++ utils/jwt.go
@@ -0,0 +1,72 @@
+package utils
+
+import (
+	"errors"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/saleh-ghazimoradi/Projectopher/config"
+	"time"
+)
+
+type Claims struct {
+	FirstName string
+	LastName  string
+	Email     string
+	Role      string
+	UserId    string
+	jwt.RegisteredClaims
+}
+
+func GenerateToken(cfg *config.Config, firstname, lastname, email, role, userId string) (accessToken, RefreshToken string, err error) {
+	accessClaims := &Claims{
+		FirstName: firstname,
+		LastName:  lastname,
+		Email:     email,
+		Role:      role,
+		UserId:    userId,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(cfg.JWT.ExpiresIn)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	at := jwt.NewWithClaims(jwt.SigningMethodES256, accessClaims)
+	accessToken, err = at.SignedString([]byte(cfg.JWT.Secret))
+	if err != nil {
+		return "", "", err
+	}
+
+	refreshClaims := &Claims{
+		FirstName: firstname,
+		LastName:  lastname,
+		Email:     email,
+		Role:      role,
+		UserId:    userId,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(cfg.JWT.RefreshTokenExpires)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	rt := jwt.NewWithClaims(jwt.SigningMethodES256, refreshClaims)
+	refreshToken, err := rt.SignedString([]byte(cfg.JWT.Secret))
+	if err != nil {
+		return "", "", err
+	}
+
+	return accessToken, refreshToken, nil
+}
+
+func ValidateToken(tokenString, secret string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (any, error) {
+		return []byte(secret), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+		return claims, nil
+	}
+	return nil, errors.New("invalid token")
+}
