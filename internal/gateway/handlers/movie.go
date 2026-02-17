863edf139d3df7c884d0b1f2c15d972ea3704242
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2026-02-17 22:06:40 +0330

    update movie handlers

--- internal/gateway/handlers/movie.go
+++ internal/gateway/handlers/movie.go
@@ -3,10 +3,12 @@ package handlers
 import (
 	"errors"
 	"github.com/julienschmidt/httprouter"
+	"github.com/saleh-ghazimoradi/Projectopher/internal/domain"
 	"github.com/saleh-ghazimoradi/Projectopher/internal/dto"
 	"github.com/saleh-ghazimoradi/Projectopher/internal/helper"
 	"github.com/saleh-ghazimoradi/Projectopher/internal/repository"
 	"github.com/saleh-ghazimoradi/Projectopher/internal/service"
+	"github.com/saleh-ghazimoradi/Projectopher/utils"
 	"net/http"
 	"strconv"
 )
@@ -82,6 +84,70 @@ func (m *MovieHandler) GetMovies(w http.ResponseWriter, r *http.Request) {
 	}
 
 	helper.PaginatedSuccessResponse(w, "Movies successfully retrieved", movies, *meta)
+}
+
+func (m *MovieHandler) AdminReviewUpdate(w http.ResponseWriter, r *http.Request) {
+	role, exists := utils.RoleFromCtx(r.Context())
+	if !exists {
+		helper.BadRequestResponse(w, "Invalid role", nil)
+		return
+	}
+
+	if role != string(domain.UserRoleAdmin) {
+		helper.UnauthorizedResponse(w, "User must be part of the admin role")
+		return
+	}
+
+	imdbId := r.URL.Query().Get("imdb_id")
+	if imdbId == "" {
+		helper.BadRequestResponse(w, "Invalid imdb_id", nil)
+		return
+	}
+
+	var payload dto.AdminReviewUpdateReq
+	if err := helper.ReadJSON(w, r, &payload); err != nil {
+		helper.BadRequestResponse(w, "Invalid payload", err)
+		return
+	}
+
+	v := helper.NewValidator()
+	dto.ValidateAdminReview(v, &payload)
+	if !v.Valid() {
+		helper.FailedValidationResponse(w, "Validation failed")
+		return
+	}
+
+	resp, err := m.movieService.UpdateAdminReview(r.Context(), imdbId, &payload)
+	if err != nil {
+		helper.InternalServerError(w, "Failed to update admin review", err)
+		return
+	}
+
+	helper.SuccessResponse(w, "Admin review successfully updated", resp)
+}
+
+func (m *MovieHandler) GetRecommendedMoviesHandler(w http.ResponseWriter, r *http.Request) {
+	userId, exists := utils.UserIdFromCtx(r.Context())
+	if !exists {
+		helper.BadRequestResponse(w, "Invalid user id", nil)
+		return
+	}
+
+	movies, err := m.movieService.GetRecommendedMovies(r.Context(), userId)
+	if err != nil {
+		helper.InternalServerError(w, "Failed to fetch recommended movies", err)
+		return
+	}
+
+	helper.SuccessResponse(w, "Recommended movies successfully retrieved", movies)
+}
+
+func (m *MovieHandler) GetGenres(w http.ResponseWriter, r *http.Request) {
+	genres, err := m.movieService.GetGenres(r.Context())
+	if err != nil {
+		helper.InternalServerError(w, "Failed to fetch genres", err)
+	}
+	helper.SuccessResponse(w, "Genres successfully retrieved", genres)
 }
 
 func NewMovieHandler(movieService service.MovieService) *MovieHandler {
